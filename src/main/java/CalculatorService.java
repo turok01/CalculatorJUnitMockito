91904b24956f3717fc07b84def9c4bdad8f7f559
turok01 <44950560+turok01@users.noreply.github.com>
2020-01-22 16:29:42 +0300

    Spy

--- src/main/java/CalculatorService.java
+++ src/main/java/CalculatorService.java
@@ -1,8 +1,8 @@
 //https://coderlessons.com/tutorials/kachestvo-programmnogo-obespecheniia/vyuchit-mokito/mockito-integratsiia-junit
 //https://howtodoinjava.com/mockito/mockito-mock-injectmocks/
 public interface CalculatorService {
-    public double add(double input1, double input2);
-    public double subtract(double input1, double input2);
-    public double multiply(double input1, double input2);
-    public double divide(double input1, double input2);
+    double add(double input1, double input2);
+    double subtract(double input1, double input2);
+    double multiply(double input1, double input2);
+    double divide(double input1, double input2);
 }
--- src/main/java/MathApplicationTester.java
+++ src/main/java/MathApplicationTester.java
@@ -16,6 +16,7 @@ import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 
 //@runWith attaches a runner with the test class to initialize the test data
 //To process annotations, we can use the built-in runner MockitoJUnitRunner
@@ -26,26 +27,33 @@ public class MathApplicationTester {
     @Before
     public void setUp(){
         mathApplication = new MathApplication();
-        calcService = mock(CalculatorService.class);
+        Calculator calculator = new Calculator();
+        calcService = spy(calculator);
         mathApplication.setCalculatorService(calcService);
     }
     @Test
-    public void testAddAndSubtract(){
-        //add the behavior of calc service to add two numbers
-        when(calcService.add(10.0,20.0)).thenReturn(30.00);
-
-        //add the behavior of calc service to subtract two numbers
-        when(calcService.subtract(20.0,10.0)).thenReturn(10.00);
-
-        //test the add functionality
+    public void testAdd(){
+        //perform operation on real object
+        // test the add functionality
         // assert that calcService.add(10,20) Equals 30
         Assert.assertEquals(mathApplication.add(10.0,20.0),30.0,0);
-
-        //test the subtract functionality
-        Assert.assertEquals(mathApplication.subtract(20.0,10.0),10.0,0);
-
-        //default call count is 1
-        verify(calcService).add(10.0,20.0);
-        verify(calcService).subtract(20.0,10.0);
+    }
+    class Calculator implements CalculatorService{
+        @Override
+        public double add(double input1, double input2){
+            return input1 + input2;
+        }
+        @Override
+        public double subtract(double input1, double input2){
+            throw new UnsupportedOperationException("Method not implemented yet!");
+        }
+        @Override
+        public double multiply(double input1, double input2){
+            throw new UnsupportedOperationException("Method not implemented yet!");
+        }
+        @Override
+        public double divide(double input1, double input2){
+            throw new UnsupportedOperationException("Method not implemented yet!");
+        }
     }
 }
