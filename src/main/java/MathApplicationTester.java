270ee82b9f1896cf4a3adb27880973302093fc6f
turok01 <44950560+turok01@users.noreply.github.com>
2020-01-22 16:58:24 +0300

    BDD Given When Then

--- src/main/java/MathApplicationTester.java
+++ src/main/java/MathApplicationTester.java
@@ -9,6 +9,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.times;
@@ -16,7 +17,8 @@ import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.reset;
+
+
 
 //@runWith attaches a runner with the test class to initialize the test data
 //To process annotations, we can use the built-in runner MockitoJUnitRunner
@@ -32,19 +34,13 @@ public class MathApplicationTester {
     }
     @Test
     public void testAdd(){
-        //add the behavior to add number
-        when(calcService.add(10.0,20.0)).thenReturn(30.0);
+        //Given
+        given(calcService.add(10.0,20.0)).willReturn(30.0);
 
-        //perform operation on real object
-        // test the add functionality
-        // assert that calcService.add(10,20) Equals 30
-        Assert.assertEquals(mathApplication.add(10.0,20.0),30.0,0);
+        //when
+        double result =calcService.add(10.0,20.0);
 
-        //reset the mock
-        reset(calcService);
-
-        // test the add functionality after resetting the mock
-        Assert.assertEquals(mathApplication.add(10.0,20.0),30.0,0);
-
+        //then
+        Assert.assertEquals(result,30.0,0);
     }
 }
